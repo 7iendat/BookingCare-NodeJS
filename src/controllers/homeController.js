c58a95dd83c47f64f420644b250bca5ea47b1c4c
7iendat <datnguyentien0803@gmail.com>
2023-09-01 20:25:04 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+import db from "../models/index"
 
-let getHomePage = (req, res) => {
-    return res.render('homePage.ejs')
+
+let getHomePage = async(req, res) => {
+   try {
+    let data = await db.User.findAll();
+        console.log("data", data);
+    return res.render('homePage.ejs', {
+        data: JSON.stringify(data)
+    })
+   } catch (e) {
+        console.log(e)
+   }
 }
 
 
---
+++ src/seeders/20230901124646-demo-user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
